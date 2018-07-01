a166ae5af2a25362cdb4ac9a9b404af42de02b2b
jaehongjun <gatsby0391@naver.com>
2018-07-01 17:47:17 +0900

    Initial commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { LoadingController } from 'ionic-angular';
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController, public loadingCtrl: LoadingController) {
+    this.presentLoading();
+  }
+
+  presentLoading(){
+    let loader = this.loadingCtrl.create({
+      content: "Please wait...",
+      duration: 3000
+    })
+    loader.present();
+  }
+}
